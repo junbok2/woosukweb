6003923ec6dee9b9f726e20f4f644b7493b45255
qwswe <qwswe1456@naver.com>
2023-11-15 16:03:08 +0900

    v1.0

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
