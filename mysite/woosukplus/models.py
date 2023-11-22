c39d073f18c26d8c0563d8f88443bf49fba13ff7
qwswe <qwswe1456@naver.com>
2023-11-22 17:02:37 +0900

    v1.0

--- mysite/woosukplus/models.py
+++ mysite/woosukplus/models.py
@@ -74,4 +74,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def str(self):
+        return self.title
